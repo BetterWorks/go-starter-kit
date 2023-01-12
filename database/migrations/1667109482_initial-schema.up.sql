1cb30ad6a00f582f0e6158d84d132060baa378c3
Jason Sites <email@jasonsites.com>
2023-01-12 16:51:13 -0700

    fix(database): corrects invalid SQL initialization script

--- database/migrations/1667109482_initial-schema.up.sql
+++ database/migrations/1667109482_initial-schema.up.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 DROP TABLE IF EXISTS "episode";
 
 CREATE TABLE IF NOT EXISTS "episode" (
-  id uuid     PRIMARY KEY DEFAULT gen_random_uuid(),
+  id          uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   season_id   uuid NOT NULL,
   title       varchar(255) NOT NULL,
   description text,
@@ -17,15 +17,15 @@ CREATE TABLE IF NOT EXISTS "episode" (
   created_by  integer NOT NULL,
   modified_on timestamptz,
   modified_by integer
-
-  CREATE INDEX episode_season_id_idx ON episode(season_id);
-  CREATE INDEX episode_status_idx ON episode(status);
 );
+
+CREATE INDEX episode_season_id_idx ON episode(season_id);
+CREATE INDEX episode_status_idx ON episode(status);
 
 DROP TABLE IF EXISTS "season";
 
 CREATE TABLE IF NOT EXISTS "season" (
-  id uuid     PRIMARY KEY DEFAULT gen_random_uuid(),
+  id          uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   title       varchar(255) NOT NULL,
   description text,
   deleted     boolean NOT NULL DEFAULT false,
@@ -36,6 +36,6 @@ CREATE TABLE IF NOT EXISTS "season" (
   created_by  integer NOT NULL,
   modified_on timestamptz,
   modified_by integer
-
-  CREATE INDEX season_status_idx ON season(status);
 );
+
+CREATE INDEX season_status_idx ON season(status);
